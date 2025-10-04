214f40a80e398432ec9fb2a2efe57a4f825ee450
keithhauzh <keithhau.thegreat@gmail.com>
2025-10-04 11:30:54 +0800

    started

--- app/src/main/java/com/keith/expensesplitter/ui/adapter/GroupsAdapter.kt
+++ app/src/main/java/com/keith/expensesplitter/ui/adapter/GroupsAdapter.kt
@@ -1,27 +1,50 @@
 package com.keith.expensesplitter.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 import com.keith.expensesplitter.data.model.Group
 import com.keith.expensesplitter.databinding.ItemLayoutGroupBinding
 
 class GroupsAdapter (
-    private var groups: List<Group>
+    private var groups: List<Group>,
+    private var onClick: (Group) -> Unit
 ): RecyclerView.Adapter<GroupsAdapter.GroupViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ):  {
-
+    ): GroupViewHolder  {
+        val binding = ItemLayoutGroupBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+            )
+        return GroupViewHolder(binding)
     }
 
+    override fun onBindViewHolder(holder: GroupViewHolder, position: Int) {
+        val group = groups[position]
+        holder.bind(group)
+    }
+
+    override fun getItemCount() = groups.size
+
+    fun setGroups(groups: List<Group>) {
+        this.groups = groups
+        notifyDataSetChanged()
+    }
 
     inner class GroupViewHolder(
         private val binding: ItemLayoutGroupBinding
     ): RecyclerView.ViewHolder(binding.root) {
         fun bind(group: Group){
-            binding.run {}
+            binding.run {
+                tvName.text = group.name
+                tvDetails.text = group.details
+                llGroup.setOnClickListener {
+                    onClick(group)
+                }
+            }
         }
     }
 }
